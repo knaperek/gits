a926e4b1d5c7b133d71b0808bd951b9c3dbe188f
Jozef Knaperek <jknaperek@gmail.com>
2012-12-09 10:56:28 +0100

    Vsade pridane from __future__ import unicode_literals

--- core/views.py
+++ core/views.py
@@ -1,1 +1,2 @@
 # Create your views here.
+from __future__ import unicode_literals
--- tp/settings.py
+++ tp/settings.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import os
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
