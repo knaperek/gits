caf3c6a520212a1e32935eab054f01f1e8d6b31f
Jozef Knaperek <jknaperek@gmail.com>
2012-12-12 17:48:42 +0100

    Deployment config changes in settings and wsgi

--- tp/wsgi.py
+++ tp/wsgi.py
@@ -15,7 +15,11 @@ framework.
 """
 from __future__ import unicode_literals
 import os
-
+import sys
+ 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+if PROJECT_ROOT not in sys.path:
+    sys.path.append(PROJECT_ROOT)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tp.settings")
 
 # This application object is used by any WSGI server configured to use this
