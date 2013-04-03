aca339dbb0f560c7e177dd4add8983fb5794e130
Jozef Knaperek <jknaperek@gmail.com>
2013-04-03 19:34:56 +0200

    Added default testing JSON string for new widgets

--- kinetic_widget/models.py
+++ kinetic_widget/models.py
@@ -26,10 +26,95 @@ class KineticField(models.TextField):
 
 # TODO: delete these models (below). They are just for testing purposes.
 
+
+example_JSON = """ 
+JSONObject= {
+"images": [
+       "images/network/router.png",
+       "images/network/switch.png",        
+       "images/network/computer.png",
+       "images/network/cloud.png",
+       "images/network/server.png",
+       "images/network/database.png"
+],
+"lines": [
+       "images/line/solid.png",
+       "images/line/dashed.png"                                        
+],
+"ports": [
+       [
+               "s0/0",
+               "s0/1",
+               "fa0/0",
+               "fa0/1",
+               "console"
+       ],
+       [
+               "fa0/0",
+               "fa0/1",
+               "fa0/2",
+               "fa0/3",
+               "fa0/4"
+       ],
+       [
+               "fast ethernet"
+       ],
+       [
+               "generic port 0",
+               "generic port 1",
+               "generic port 2",
+               "generic port 3"
+       ],
+       [
+               "fa0/0",
+               "fa0/1"
+       ],
+       [
+               "fa0/0",
+               "fa0/1"
+       ]
+],
+"port_limits": [
+       [
+               1,
+               1,
+               1,
+               1,
+               1
+       ],
+       [
+               1,
+               1,
+               1,
+               1,
+               1
+       ],
+       [
+               1
+       ],
+       [
+               1,
+               1,
+               1,
+               1
+       ],
+       [
+               1,
+               1
+       ],
+       [
+               1,
+               1
+       ]
+]
+};
+
+
+"""
 class TestWidgetSimple(models.Model):
-	jeden = KineticField(default="<some_json_data>")
+	jeden = KineticField(default=example_JSON)
 
 class TestWidgetMulti(models.Model):
-    jeden = KineticField()
-    druhy = KineticField()
-    treti = KineticField()
+    jeden = KineticField(default=example_JSON)
+    druhy = KineticField(default=example_JSON)
+    treti = KineticField(default=example_JSON)
