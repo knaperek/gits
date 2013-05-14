18c9ee2116abb916410763c4d8b9ec32939c10ea
Jozef Knaperek <jknaperek@gmail.com>
2013-05-14 20:49:54 +0200

    Changed base template for student interface; added user password change view

--- tp/urls.py
+++ tp/urls.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,10 +18,14 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^$', lambda request: TemplateView.as_view(template_name='index.html')(request)),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='user/index.html')),
 	url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='accounts-logout'),
+    url(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change', name='accounts-password-change'),
+    url(r'^accounts/password_change_done/$', 'django.contrib.auth.views.password_change_done'),
+    url(r'^accounts/$', RedirectView.as_view(url='/exam')),
 )
 
 from django.conf import settings
