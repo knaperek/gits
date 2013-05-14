cb3d2459ba93fff931d0796a743e14599b5154a9
Jozef Knaperek <jknaperek@gmail.com>
2013-05-14 18:43:19 +0200

    Added main/index page template

--- tp/urls.py
+++ tp/urls.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,6 +16,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    # url(r'^$', lambda request: TemplateView.as_view(template_name='index.html')(request)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 	url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
